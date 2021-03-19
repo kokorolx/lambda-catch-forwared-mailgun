dce5850a356aeb68697223c2859a8c520047cc2e
Kokoro <kokoro.lehoang@gmail.com>
2021-03-19 15:16:12 +0700

    update lambda sendmail via smtp

--- handler.js
+++ handler.js
@@ -7,71 +7,55 @@ const mg = require('nodemailer-mailgun-transport');
 const multer = require('multer')
 const mailgun = require("mailgun-js");
 
+const port = 3000
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// 332629e0e53b7008773e4e21696c171d-73e57fef-9c865c030
-
-
-
-app.post("/forward-mail", multer().none(), async (req, res, next) => {
+app.post("/forward-mail", multer().any(), async (req, res, next) => {
   const options = {
-    host: "mail.tradesorg.team",
+    host: process.env.SMTP_HOST,
     port: 587,
-    secure: true, // upgrade later with STARTTLS
     auth: {
-      user: "mailgun@xtradesorg.team",
-      pass: "j{muLfKSR90"
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PWD
+    },
+    tls:{
+      ciphers:'SSLv3'
     }
   }
   
-  
   const nodemailerMailgun = nodemailer.createTransport(options);
+  
+  nodemailerMailgun.verify(function(error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });
+
 
   try {
     console.log("=============req", req)
     console.log("=============req.apiGateway", req.apiGateway)
 
 
-    const email = req.body.recipient;
-    const event = req.body.event;
-
 
     console.log("===body===", req.body)
-    console.log("===recipient===", email)
-    console.log("===event===", event)
-
-
-    const _mail = req.apiGateway.event.body
-    const decodeMail = decode(_mail)
-  
-    console.log("req === ", _mail)
-    console.log("\n decode === ", decodeMail)
-  
 
     let recipients = req.body.recipient
 
-
-    if(Array.isArray(recipients)){
-      console.log("====array of recipient")
-      recipients = recipients.map(mail => {
-        return mail.replace(/@tradesorg.team/, '@mail.tradesorg.team')
-      })
-
-    }else {
-      recipients = recipients.replace(/@tradesorg.team/, '@mail.tradesorg.team')
-    }
-
-
-    nodemailerMailgun.sendMail({
+    const emailContent = {
       from: req.body.from,
       to: recipients,
-      subject: `${req.body.subject} ${new Date()}`,
-      html: `${req.body['body-html']} <b>${new Date()}</b>`,
+      subject: `${req.body.subject}`,
+      html: `${req.body['body-html']}`,
       text: req.body['body-plain']
-    }).then(response => {
+    }
+
+    nodemailerMailgun.sendMail(emailContent).then(response => {
       console.log(response)
       return res.status(200).json({
         message: "sent",
@@ -90,33 +74,16 @@ app.post("/forward-mail", multer().none(), async (req, res, next) => {
   } catch (error) {
     console.log(`==== ${new Date()}`)
     console.log("===== master of error -- ", error)
+    return res.status(400).json({
+      message: "somethings went wrong",
+      error: error
+    })
   }
-
-
-  console.log(`===========log at ==== ${new Date()}`)
-
-
 })
 
 
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from root!",
-  });
-});
-
-app.get("/hello", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
-});
-
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
-});
-
-
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
 
 module.exports.handler = serverless(app);
